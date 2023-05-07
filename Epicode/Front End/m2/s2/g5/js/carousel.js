af326b6d4d5bd4925fa240dbbc0a9f5624d541e2
daviaprea <apredav@gmail.com>
2023-05-07 22:40:37 +0200

    Risoluzione bug carousel arrow

--- Epicode/Front End/m2/s2/g5/js/carousel.js
+++ Epicode/Front End/m2/s2/g5/js/carousel.js
@@ -4,18 +4,12 @@ let firstCardWidth = row.querySelector(".col").offsetWidth;
 let rowChildren=[...row.children];
 let isDragging=false, startX, startScrollLeft;
 
-let cardPerView=Math.round(row.offsetWidth/firstCardWidth);
-rowChildren.slice(-cardPerView).reverse().forEach(card=>{
-    row.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-
-rowChildren.slice(0, cardPerView).forEach(card=>{
-    row.insertAdjacentHTML("beforeend", card.outerHTML);
-});
+console.log(arrowBtn);
 
 arrowBtn.forEach(btn => {
     btn.addEventListener("click", ()=>{
-        row.scrollLeft += btn.id==="left-arr" ? -firstCardWidth : firstCardWidth;
+        console.log(btn.classList[2]);
+        row.scrollLeft += btn.classList[2]=="left-arr" ? -firstCardWidth : firstCardWidth;
     });
 });
 
@@ -34,23 +28,6 @@ let dragStop=()=>{
     isDragging=false;
 }
 
-let infiniteScroll=()=>{
-    if(row.scrollLeft === 0)
-    {
-        row.classList.add("no-transition");
-        row.scrollLeft=row.scrollWidth-(2*row.offsetWidth);
-        row.classList.remove("no-transition");
-    }
-
-    else if(Math.floor(row.scrollLeft) === row.scrollWidth - row.offsetWidth)
-    {
-        row.classList.add("no-transition");
-        row.scrollLeft=row.offsetWidth;
-        row.classList.remove("no-transition");
-    }
-}
-
 row.addEventListener("mousedown", dragStart);
 row.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
-row.addEventListener("scroll", infiniteScroll);
