81141bc5aed3c3d72823b1b4038647bb4d2adcc5
daviaprea <apredav@gmail.com>
2023-05-09 17:07:51 +0200

    Risoluzione bug

--- Epicode/Front End/m2/s3/g2/script.js
+++ Epicode/Front End/m2/s3/g2/script.js
@@ -1,18 +1,23 @@
-let cont=0;
 document.getElementById("sub").addEventListener("click", ()=>{
+    if(localStorage.getItem("cont")==null) localStorage.setItem("cont", "0");
+
+    let cont=Number(localStorage.getItem("cont"));
     let pers=document.getElementById("nameInput");
     localStorage.setItem(`Persona ${cont}`, pers.value);
-    cont++;
     pers.value="";
-    document.getElementById("prevPerson").innerText=localStorage.getItem(`Persona ${cont-1}`);
+    document.getElementById("prevPerson").innerText=localStorage.getItem(`Persona ${cont}`);
+    cont++;
+    localStorage.setItem("cont", cont);
 });
 
 document.getElementById("remove").addEventListener("click", ()=>{
-    if(cont>0)
+    let cont=Number(localStorage.getItem("cont"));
+    if(cont>=0)
     {
         localStorage.removeItem(`Persona ${cont-1}`);
         cont--;
         document.getElementById("prevPerson").innerText=localStorage.getItem(`Persona ${cont-1}`);
+        localStorage.setItem("cont", cont);
     }
 });
 
@@ -21,7 +26,8 @@ let spanTimer=document.getElementById("timer");
 setInterval(()=>{
     if(sessionStorage.getItem("time")==null) sessionStorage.setItem("time", "0");
 
-    else{
+    else
+    {
         let time=Number(sessionStorage.getItem("time"));
         spanTimer.innerText=`${time}s`;
         time++;
