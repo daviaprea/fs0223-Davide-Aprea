d2923f21f192fc7914a92824375b685a57ef3e80
daviaprea <apredav@gmail.com>
2023-05-26 11:54:36 +0200

    Es. quasi finito

--- Epicode/Front End/m3/g5/script.js
+++ Epicode/Front End/m3/g5/script.js
@@ -3,6 +3,7 @@ var Smartphone = /** @class */ (function () {
         this.carica = 0;
         this.numeroChiamate = 0;
         this.costoMinuto = 0.2;
+        this.registroChiamate = [];
     }
     Smartphone.prototype.ricarica = function (euro) {
         this.carica += euro;
@@ -15,12 +16,30 @@ var Smartphone = /** @class */ (function () {
     };
     Smartphone.prototype.chiamata = function (min) {
         if (this.costoMinuto * min < this.carica) {
+            this.registroChiamate.push({
+                id: this.numeroChiamate,
+                durata: min,
+                data: new Date()
+            });
             this.carica -= this.costoMinuto * min;
             this.numeroChiamate++;
         }
     };
     Smartphone.prototype.azzeraChiamate = function () {
         this.numeroChiamate = 0;
+        this.registroChiamate = [];
+    };
+    Smartphone.prototype.mostraRegistroChiamate = function () {
+        return this.registroChiamate;
+    };
+    Smartphone.prototype.filtraChiamatePerDataOra = function (dataFilter) {
+        return this.registroChiamate.filter(function (chiamata) { return chiamata.data.toLocaleString() == dataFilter.toLocaleString(); });
     };
     return Smartphone;
 }());
+var myPhone = new Smartphone();
+myPhone.ricarica(50);
+myPhone.chiamata(8);
+console.log(myPhone.getNumeroChiamate());
+console.log(myPhone.numero404());
+console.log(myPhone.mostraRegistroChiamate());
--- Epicode/Front End/m3/g5/script.ts
+++ Epicode/Front End/m3/g5/script.ts
@@ -1,19 +1,31 @@
+type Registro={
+    id:number;
+    durata:number;
+    data:Date;
+}
+
 interface Phone{
     carica:number;
     numeroChiamate:number;
     costoMinuto:number;
+    registroChiamate:{}[];
+
     ricarica(euro:number):void;
     numero404():string;
     getNumeroChiamate():number;
     chiamata(min:number):void;
     azzeraChiamate():void;
+    mostraRegistroChiamate():Registro[];
+    filtraChiamatePerDataOra(data:Date):Registro[];
 }
 
 class Smartphone implements Phone{
     carica: number=0;
     numeroChiamate: number=0;
     costoMinuto: number=0.2;
-    ricarica(euro: number): void {
+    registroChiamate:Registro[]=[];
+    
+    ricarica(euro:number): void {
         this.carica+=euro;
     }
     numero404(): string {
@@ -22,14 +34,33 @@ class Smartphone implements Phone{
     getNumeroChiamate(): number {
         return this.numeroChiamate;
     }
-    chiamata(min: number): void {
+    chiamata(min:number): void {
         if(this.costoMinuto*min<this.carica)
         {
+            this.registroChiamate.push({
+                id: this.numeroChiamate,
+                durata: min,
+                data: new Date()
+            });
             this.carica-=this.costoMinuto*min;
             this.numeroChiamate++;
         }
     }
     azzeraChiamate(): void {
         this.numeroChiamate=0;
+        this.registroChiamate=[];
+    }
+    mostraRegistroChiamate(): Registro[] {
+        return this.registroChiamate;
+    }
+    filtraChiamatePerDataOra(dataFilter: Date): Registro[] {
+        return this.registroChiamate.filter(chiamata=>chiamata.data.toLocaleString()==dataFilter.toLocaleString());
     }
 }
+
+let myPhone=new Smartphone();
+myPhone.ricarica(50);
+myPhone.chiamata(8)
+console.log(myPhone.getNumeroChiamate());
+console.log(myPhone.numero404());
+console.log(myPhone.mostraRegistroChiamate());
