8a8eca5f5f8b3e5dcc8fc749bac016adaf9930b9
daviaprea <apredav@gmail.com>
2023-06-04 13:09:25 +0200

    Modifica stili

--- Epicode/Front End/m3/s2/g5/src/app/parts/pages/completati/completati.component.ts
+++ Epicode/Front End/m3/s2/g5/src/app/parts/pages/completati/completati.component.ts
@@ -3,7 +3,7 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'app-completati',
   templateUrl: './completati.component.html',
-  styleUrls: ['./completati.component.scss']
+  styleUrls: ['../todo/todo.component.scss']
 })
 export class CompletatiComponent{
 }
